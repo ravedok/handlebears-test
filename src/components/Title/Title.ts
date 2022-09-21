23649ead54a4a74a2475b3777f54dc8796d41520
Jose Ramon Macia Gaitan <joseramon.macia@atlasinformatica.com>
2022-09-21 22:33:24 +0000

    Styled Title

--- src/components/Title/Title.ts
+++ src/components/Title/Title.ts
@@ -1,6 +1,7 @@
 import Handlebars from "handlebars";
+import "./styles.scss";
 
-const template = "<h1>{{content}}</h1>";
+const template = `<h1 class="title">{{content}}</h1>`;
 
 Handlebars.registerHelper("Title", (options) =>
   Handlebars.compile(template)({ content: options.fn(this) })
